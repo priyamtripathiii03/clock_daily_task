babb6a821718475eec89f6cc7cf5241139a9d997
priyamtripathiii03 <priyamtripathiii03@gmail.com>
2024-08-06 14:25:27 +0530

    06-08-2024

--- lib/clock.dart
+++ lib/clock.dart
@@ -31,9 +31,25 @@ class _ClockState extends State<Clock> {
             ),
         ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('01:00'),
+            Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                    '${(dateTime.hour%12).toString().padLeft(2,'0')} : ${(dateTime.minute).toString().padLeft(2,'0')}',style: TextStyle(fontSize: 60,color: Colors.white,fontWeight: FontWeight.bold),),
+               SizedBox(width: 10,),
+                Text('${dateTime.second.toString().padLeft(2,'0')} : ${(dateTime.hour>=12)?'PM':'AM'}',style: (TextStyle(color: Colors.white,height: -2,fontWeight: FontWeight.bold,fontSize: 22)),),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('${days[dateTime.weekday-1]} ${dateTime.day} ${months[dateTime.month-1]}',style: TextStyle(color: Colors.white,fontSize: 22),)
+              ],
+            ),
+            Spacer(flex: 4,),
           ],
         ),
       ),
